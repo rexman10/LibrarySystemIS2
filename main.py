9426699788be1d65a725ff0e6dc5575321afa390
Juan Guadalupe <jaguadal@espol.edu.ec>
2023-12-06 21:06:46 -0500

    prueba cambio

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ class Book:
         self.title = title
         self.author = author
         self.availability = availability
-
+        
+        
     def __str__(self):
         return f"Book{{code='{self.code}', title='{self.title}', author='{self.author}', availability={self.availability}}}"
 
